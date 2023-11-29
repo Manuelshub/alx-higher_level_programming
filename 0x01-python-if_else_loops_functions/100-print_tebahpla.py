754e7c53ea27123c01890585f556e193c27b845e
Emmanuel <kcemmy03@gmail.com>
2023-11-29 05:55:04 +0100

    Fixed string formatting issue

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for ch in range(122, 96, -1):
-    print(chr(ch) if ch % 2 == 0 else chr(ch - 32), end='')
+    print("{:c}".format(ch) if ch % 2 == 0 else chr(ch - 32), end='')
