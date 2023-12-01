8b576c7612981da54c47d3e81f04acb1636461b8
Emmanuel <kcemmy03@gmail.com>
2023-12-01 11:49:03 +0100

    Fixed a requirement issue

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
-num_args = len(sys.argv) - 1
-if num_args == 0:
-    print(num_args, "arguments.")
-else:
-    if num_args == 1:
-        print(num_args, "argument:")
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print(num_args, "arguments.")
     else:
-        print(num_args, "arguments:")
-    for i in range(num_args):
-        args = sys.argv[i + 1]
-        print("{}:".format(i + 1), args)
+        if num_args == 1:
+            print(num_args, "argument:")
+        else:
+            print(num_args, "arguments:")
+        for i in range(num_args):
+            args = sys.argv[i + 1]
+            print("{}:".format(i + 1), args)
