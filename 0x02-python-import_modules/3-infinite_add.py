c9797153b915d7d8305cc3bc181983194a12c344
Emmanuel <kcemmy03@gmail.com>
2023-12-01 12:22:55 +0100

    Fixed a requirement issue

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
-num_args = len(sys.argv) - 1
-if num_args == 0:
-    print(num_args)
-else:
-    total_sum = 0
-    for i in range(num_args):
-        args = int(sys.argv[i + 1])
-        total_sum += args
-    print(total_sum)
+
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print(num_args)
+    else:
+        total_sum = 0
+        for i in range(num_args):
+            args = int(sys.argv[i + 1])
+            total_sum += args
+        print(total_sum)
