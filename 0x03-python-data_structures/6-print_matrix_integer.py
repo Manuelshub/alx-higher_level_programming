fe168e433c2345214463135c5d1165e24e6deb24
Emmanuel <kcemmy03@gmail.com>
2023-12-04 17:05:12 +0100

    Fixed a requirement issue

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print("{:d}".format(element), end=' ')
+            num = element
+            print("{:d}".format(num), end=' ')
         print()
