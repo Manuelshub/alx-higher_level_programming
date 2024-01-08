0e95f52e8f298faf06d3ee153a1812fbd332313e
Emmanuel <kcemmy03@gmail.com>
2024-01-08 09:38:37 +0100

    Modified my function and added a test case

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,15 +8,17 @@ def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix.
     """
-    sizeof_matrix = len(matrix[0])
     new_matrix = []
+    msg_1 = "matrix must be a matrix (list of lists) of integers/floats"
     for i in range(len(matrix)):
         lst = []
         for j in range(len(matrix[i])):
-            if sizeof_matrix != len(matrix[i]):
-                raise TypeError("Each row of the matrix must have the same size")
-            if not isinstance(matrix[i][j], (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            if not (len(matrix[0]) == len(matrix[i])):
+                msg_2 = "Each row of the matrix must have the same size"
+                raise TypeError(msg_2)
+            if (not isinstance(matrix[i][j], (int, float)) or
+            not isinstance(matrix, list)):
+                raise TypeError(msg_1)
             if not isinstance(div, (int, float)):
                 raise TypeError("div must be a number")
             if div == 0:
@@ -25,4 +27,3 @@ def matrix_divided(matrix, div):
             lst.append(elem)
         new_matrix.append(lst)
     return new_matrix
-
