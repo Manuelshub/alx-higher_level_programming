c306ec83d9eb2cc63f50d5a3fea02bb3f8ace967
Emmanuel <kcemmy03@gmail.com>
2024-01-09 11:30:18 +0100

    Removed a trailing whitespace

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -7,7 +7,7 @@ Function that returns True if the object is an instance of the class
 def is_kind_of_class(obj, a_class):
     """
     Returns True if the object is an instance of the class,
-    Or an instance of a class that inherited from the class 
+    Or an instance of a class that inherited from the class
     """
     if isinstance(obj, a_class):
         return True
