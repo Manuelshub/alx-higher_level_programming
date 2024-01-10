2ec4f147b93a39b3554889176633a81131514e67
Emmanuel <kcemmy03@gmail.com>
2024-01-10 18:43:26 +0100

    A class that defines a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bi/python3
+"""
+A class Student
+"""
+
+
+class Student:
+    """ Class that defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Method that initializes the class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ This method retriees a dictionary representation of a
+        class instance.
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
