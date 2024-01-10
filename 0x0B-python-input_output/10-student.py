34bfd85044f56643a00b8b5f5326ade74b9f4890
Emmanuel <kcemmy03@gmail.com>
2024-01-10 18:59:41 +0100

    Defines a class student based on 9-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A class Student
+"""
+
+
+class Student:
+    """ Class that defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Method that initializes the class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ This method retriees a dictionary representation of a
+        class instance.
+        """
+        if attrs is None:
+            return {
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'age': self.age
+            }
+        else:
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs if hasattr(self, attr)}
