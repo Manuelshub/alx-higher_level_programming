515736caf8b59e7830fcea59a32e9bef1235e621
Emmanuel <kcemmy03@gmail.com>
2024-01-10 07:15:28 +0100

    Modified the function

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     filename = "add_item.json"
-    args = load_from_json_file(filename)
-    if args == FileNotFoundError:
+    try:
+        args = load_from_json_file(filename)
+    except FileNotFoundError:
         args = []
     args.extend(sys.argv[1:])
     save_to_json_file(args, filename)
