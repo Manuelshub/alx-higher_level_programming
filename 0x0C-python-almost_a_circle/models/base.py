57a52dc35735a13f8cf69e61d85fe0b702ddb291
Emmanuel <kcemmy03@gmail.com>
2024-01-16 04:33:12 +0100

    Created a class base and an empty folder that indicates a python packagee

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" My class base
+"""
+
+
+class Base:
+    """ This class will be the base of all other classes
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initializes the class base
+        id: (int) the id of the class
+        """    
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
