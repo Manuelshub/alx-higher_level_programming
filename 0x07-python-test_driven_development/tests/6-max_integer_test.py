7aa579fb1713b460f3c40e794d2630d0d23506e7
Emmanuel <kcemmy03@gmail.com>
2024-02-20 19:34:07 +0100

    Added some more test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,11 +8,48 @@ class TestMaxInteger(unittest.TestCase):
     """Test cases for the function that returns the max of
         numbers in a list.
     """
-    def test_max_integer(self):
-        """Testing the function max_integer."""
+    def test_positive_ints(self):
+        """Testing the function with positive integers."""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([7, 45, 23, 65]), 65)
+
+    def test_empty_list(self):
+        """Testing the function with an empty list"""
         self.assertEqual(max_integer([]), None)
+
+    def test_string(self):
+        """Testing the function with a string."""
+        self.assertEqual(max_integer("Hello"), 'o')
+
+    def test_empty_string(self):
+        """Testing the function with an empty string"""
+        self.assertEqual(max_integer(""), None)
+
+    def test_single_element(self):
+        """Testing the function with a single element."""
+        self.assertEqual(max_integer([10]), 10)
+
+    def test_string_list(self):
+        """Testing the function with a list of string."""
+        self.assertEqual(max_integer(["Hello", "World"]), "World")
+
+    def test_negative_ints(self):
+        """Testing the function with negative integers"""
+        self.assertEqual(max_integer([-10, -45, -34, -20]), -10)
+
+    def test_mixed_ints(self):
+        """Testing with a list of both positive and negative
+            integers
+        """
+        self.assertEqual(max_integer([-10, -39, 8, 5, 34]), 34)
+
+    def test_floats(self):
+        """Testing the function with floats"""
+        self.assertEqual(max_integer([19.2, 23.5, 34.4]), 34.4)
+
+    def test_large_ints(self):
+        """Testing the function with large positive integers"""
+        self.assertEqual(max_integer([100000, 190000]), 190000)
 
 
 if __name__ == "__main__":
