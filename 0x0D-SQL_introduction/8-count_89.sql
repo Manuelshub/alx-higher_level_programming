eb8ed43279d206ac051b4d987f4929a8c7bb54fd
Emmanuel <kcemmy03@gmail.com>
2024-03-15 12:37:18 +0100

    Added the where statement

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,4 @@
 -- This script displays the number of records with id=89 in the table first_table of a database in my MySQL server.
-SELECT COUNT(id = 89)
-FROM first_table;
+SELECT COUNT(id)
+FROM first_table
+WHERE id = 89;
