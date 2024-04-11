3769267eef7334b69276c9edd378a042c53f332c
Emmanuel <kcemmy03@gmail.com>
2024-04-11 03:40:24 +0100

    Script that searches for the biggest integer

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+if (args.length < 2) {
+  console.log('0');
+}
+
+const numbers = args.map(Number);
+let max = -Infinity;
+let secondMax = -Infinity;
+for (let i = 0; i < args.length; i++) {
+  if (numbers[i] > max) {
+    secondMax = max;
+    max = numbers[i];
+  } else if (numbers[i] > max && numbers[i] !== max) {
+    secondMax = numbers[i];
+  }
+}
+console.log(secondMax);
