2982c4ac803e76d62bc6ac959bb6abb726d24508
Emmanuel <kcemmy03@gmail.com>
2024-04-11 10:17:25 +0100

    Added an else case

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,19 +3,19 @@
 const args = process.argv.slice(2);
 if (args.length < 2) {
   console.log('0');
-}
-
-const numbers = args.map(Number);
-let max = Math.max(numbers[0], numbers[1]);
-let secondMax = Math.min(numbers[0], numbers[1]);
-for (let i = 2; i < args.length; i++) {
-  if (numbers[i] > max) {
-    secondMax = max;
-    max = numbers[i];
-  } else if (numbers[i] > secondMax && numbers[i] !== max) {
-    secondMax = numbers[i];
-  } else if (max === secondMax && secondMax !== numbers[i]) {
-    secondMax = numbers[i];
+} else {
+  const numbers = args.map(Number);
+  let max = Math.max(numbers[0], numbers[1]);
+  let secondMax = Math.min(numbers[0], numbers[1]);
+  for (let i = 2; i < args.length; i++) {
+    if (numbers[i] > max) {
+      secondMax = max;
+      max = numbers[i];
+    } else if (numbers[i] > secondMax && numbers[i] !== max) {
+      secondMax = numbers[i];
+    } else if (max === secondMax && secondMax !== numbers[i]) {
+      secondMax = numbers[i];
+    }
   }
+  console.log(secondMax);
 }
-console.log(secondMax);
