254f3924c0e65f93871bed1f021d4122b7ab6e2f
Emmanuel <kcemmy03@gmail.com>
2024-05-01 03:16:54 +0100

    Changed code formatting

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,13 +5,11 @@
 if __name__ == '__main__':
     import urllib.request
 
-    url = 'https://alx-intranet.hbtn.io/status'
-
-    with urllib.request.urlopen(url) as hbtn_page:
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as hbtn_page:
         page = hbtn_page.read()
         page_fmt = page.decode('utf-8')
-
-    print('Body response:')
-    print('\t- type:', type(page))
-    print('\t- content', page)
-    print('\t- utf8 content:', page_fmt)
+        print('Body response:')
+        print('\t- type:', type(page))
+        print('\t- content', page)
+        print('\t- utf8 content:', page_fmt)
