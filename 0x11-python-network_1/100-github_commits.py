3a93d2507bac504f8d7b9165408afdb1cca36c09
Emmanuel <kcemmy03@gmail.com>
2024-05-01 12:01:20 +0100

    Script that fetches commit hash and the committer name from a repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script that prints the commit hash and the committer
+"""
+from requests.auth import HTTPBasicAuth
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
+    datas = response.json()
+
+    for data in datas:
+        sha = data.get('sha')
+        committer = data['commit']['committer'].get('name')
+        print(f"{sha}: {committer}")
