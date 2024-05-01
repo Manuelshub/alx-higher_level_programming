989cea52366b61349925bbb13fc0d6d0910ac806
Emmanuel <kcemmy03@gmail.com>
2024-05-01 04:13:04 +0100

    Script that displays the body of the response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Script that sends a request to a URL and displays the body of
+    the response (decoded in utf-8).
+"""
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    req = Request(url)
+    try:
+        urlopen(req)
+    except HTTPError as e:
+        print(e.read().decode('utf-8'))
