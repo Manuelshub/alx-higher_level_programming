bcd35d52a3ef81ae3797d2190213efd4c9694f5e
Emmanuel <kcemmy03@gmail.com>
2024-05-01 03:09:55 +0100

    Adjusted formatting

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     with urllib.request.urlopen(req) as hbtn_page:
         page = hbtn_page.read()
         page_fmt = page.decode('utf-8')
-        print('Body response:')
-        print('\t- type:', type(page))
-        print('\t- content', page)
-        print('\t- utf8 content:', page_fmt)
+
+    print('Body response:')
+    print('\t- type:', type(page))
+    print('\t- content', page)
+    print('\t- utf8 content:', page_fmt)
