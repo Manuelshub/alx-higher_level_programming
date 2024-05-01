f03b7da0640e4cd2d52bc77a6d27e06c7993e001
Emmanuel <kcemmy03@gmail.com>
2024-05-01 03:07:27 +0100

    Script that fetches from a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Script that fetches 'https://alx-intranet.hbtn.io/status' """
+
+
+if __name__ == '__main__':
+    import urllib.request
+
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as hbtn_page:
+        page = hbtn_page.read()
+        page_fmt = page.decode('utf-8')
+        print('Body response:')
+        print('\t- type:', type(page))
+        print('\t- content', page)
+        print('\t- utf8 content:', page_fmt)
