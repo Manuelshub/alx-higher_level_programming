24248ceb3fcd4835f6eb79f47d21f3919321024a
Emmanuel <kcemmy03@gmail.com>
2024-05-22 19:21:54 +0100

    Fix requirement issue

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 
 // fs: file system
-const fs = require('fs')
+const fs = require('fs');
 
-const filename = process.argv[2]
-const content = process.argv[3]
+const filename = process.argv[2];
+const content = process.argv[3];
 
 fs.writeFile(filename, content, err => {
   if (err) {
     console.error(err);
-    return;
-	}
-})
+  }
+});
