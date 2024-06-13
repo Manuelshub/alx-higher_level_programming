041df60c362aec42d06a0ddfec4de187f357f02e
Emmanuel <kcemmy03@gmail.com>
2024-06-13 04:31:56 +0100

    Script that creates a new user and grants it permission

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- This Script creates a user and grants it privileges
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+WITH GRANT OPTION;
